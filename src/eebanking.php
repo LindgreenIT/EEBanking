3efe4926ffa818789a314a96488ce0810b7e3659
Thomas Bentsen <C5286972@c02cc201md6r.dhcp.cph.sap.corp>
2022-04-26 12:00:36 +0200

    update
    just a update

--- src/eebanking.php
+++ src/eebanking.php
@@ -1,16 +1,19 @@
 <?php
 namespace eeBanking;
-use illuminate/database;
+use doctrine\DBAL\DriverManager;
 
-class eebanking {
+class eeBanking {
 
-    $db;
+    private $db;
 
     public function __construct() {
-
-        $this->db = new Database();
-
+        $this->db = DriverManager::getConnection(array(
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'eebanking',
+            'user' => 'root',
+            'password' => '',
+        ));
     }
-
 
 }
