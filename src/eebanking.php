5496649ceae71cf253dfc2891eb05eb44ad86cb2
Thomas Bentsen <C5286972@c02cc201md6r.dhcp.cph.sap.corp>
2022-04-26 15:02:11 +0200

    Update eebanking.php

--- src/eebanking.php
+++ src/eebanking.php
@@ -16,4 +16,36 @@ class eeBanking {
         ));
     }
 
+    public function addAccout($account) {
+        $this->db->insert('accounts', $account);
+    }
+
+    public function getAccountById($account_id) {
+       if (is_numeric($account_id)) {  
+             if ($account_id == null) {
+                $sql = "SELECT * FROM accounts ORDER BY account_id DESC";
+                $result = $stmt->executequery();
+            } else {
+                $sql = "SELECT * FROM accounts WHERE account_id = :account_id";
+                $stmt = $this->db->prepare($sql);
+                $stmt->bindValue('account_id', $account_id);
+                $result = $stmt->executequery();
+            }
+            return $result;
+        } else {
+            return false;
+        }
+    }
+
+    public function getAccountByUserId($user_id) {
+        if (is_numeric($user_id) && $user_id != null && \strlen($user_id) == 10) {
+            $sql = "SELECT * FROM accounts WHERE user_id = :user_id";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindValue('user_id', $user_id);
+            $result = $stmt->executequery();
+            return $result;
+        } else {
+            return false;
+        }
+    }
 }
